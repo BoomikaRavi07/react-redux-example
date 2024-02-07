be9fb2f989f7f6d94ac74fb0075fe7d02c3390f5
Boomika <boomikaravi0705@gmail.com>
2024-02-07 11:36:23 +0530

    redux prg

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import isLoggedReducer from "./isLogged";
+import counterReducer from "./counter";
+import { createStore,combineReducers } from "redux";
+
+const store = combineReducers({
+    login:isLoggedReducer,
+    counter:counterReducer
+})
+export default store
